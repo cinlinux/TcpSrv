cc9fb775919ad48ef90d8add2621905dac39cc47
Slavjan <khaidayev@gmail.com>
2019-03-21 11:11:14 +0700

    binding, accepting, read message, modifying, ricent

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -9,8 +10,12 @@ int main()
 {
     int sock, listener;
     struct sockaddr_in addr;
-    char buf[1024];
+    char buf[1024],
+         sendBuf[1024];
+
     int bytes_read;
+
+    short hport = 3425;
 
     listener = socket(AF_INET, SOCK_STREAM, 0);
     if( listener < 0 )
@@ -20,7 +25,7 @@ int main()
     }
 
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(3425);
+    addr.sin_port = htons(hport);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
     if(bind(listener, (struct sockaddr*)&addr, sizeof(addr))<0)
@@ -29,8 +34,8 @@ int main()
         exit(2);
     }
 
-    listener(listener, 1);
-
+    listen(listener, 1);
+    printf("listen %d\n", hport);
     while(1)
     {
         sock = accept(listener, NULL, NULL);
@@ -39,12 +44,20 @@ int main()
             perror("accept");
             exit(3);
         }
-
+        printf("sodket \n");
         while(1)
         {
+
+            memset(sendBuf, '\0', 1024);
+            memset(buf, '\0', 1024);
             bytes_read = recv(sock, buf, 1024, 0);
-            if(bytes_read <= 0) break;
-            send(sock, strcat(buf, "123"), bytes_read, 0);
+
+            if(bytes_read <= 0)
+                break;
+
+            strncpy(sendBuf, buf, bytes_read - 1);
+            strcat(sendBuf, "123\n\0");
+            send(sock, sendBuf, sizeof(sendBuf), 0);
         }
         close(sock);
     }
